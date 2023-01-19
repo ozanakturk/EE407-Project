17e1e76eb8d7efeb94c13c2a53c7b550d2e18978
ParisaRe <pari35r@yahoo.com>
2023-01-19 15:42:41 +0300

    Modified version of "On Off Control" code

--- ee407project/ee407project.ino
+++ ee407project/ee407project.ino
@@ -6,12 +6,11 @@
 
 #include <ACS712.h>
 
-int b;
-int curDuty; // A variable that holds the current duty cycle (hold a value in the range of 0-255 for analogWrite)
-int mVperAmp = 66;  // The sensing accuracy of the 30A module
-int SensorVout; // The raw sensor output (analogRead's output is between 0-1023)
-double AmpThrough; // The current passing through the motor
-double RefAmp; // Reference for control
+ACS712 sensor(ACS712_30A, A0); // Sensor decleration
+
+int curDuty = 0; // A variable that holds the current duty cycle (hold a value in the range of 0-255 for analogWrite)
+double AmpThrough = 0.0; // The current passing through the motor
+double RefAmp = 0.0; // Reference for control
 
 void soft_start(){ // It increases the duty cycle each seconds by a small amount up to 60
   int ss_duty = 0; // A variable holding the soft start duty cycle (0-255)
@@ -31,78 +30,58 @@ void setup() {
   pinMode(BUTTON_2, INPUT);   //BUTON 2
   Serial.begin(9600);
 
-  soft_start();  
-}
+  // If you are not sure that the current through the sensor will not leak during calibration - comment out this method
+  Serial.println("Calibrating... Ensure that no current flows through the sensor at this moment");
+  sensor.calibrate();
+  Serial.println("Done!");
 
-void current_rms_read(){ // Measures the current and calculates its rms value
-  SensorVout = analogRead(SENSOR_PIN); // The sensor output is read (0-1023)
-  AmpThrough = abs((((SensorVout / 1023.0) * 5000) - 2500) / mVperAmp) - 0.05; // The calculation for the current is done according to the datasheet of ACS712
+  soft_start();
 }
 
 void loop() {
-  current_rms_read();
+  AmpThrough = sensor.getCurrentAC(); // Read the rms value of the current
 
   // ON_OFF_control
-  current_rms_read();
-
-  if (digitalRead(BUTTON_1) == 1)  // Button 1 is pressed
-  {
+  if (digitalRead(BUTTON_1) == 1){  // Button 1 is pressed
     RefAmp = 1.5;
+    Serial.println(String("New Reference Value: ") + RefAmp); // Print RefAmp
   }
-  if (digitalRead(BUTTON_2) == 1)  // Button 2 is pressed
-  {
+  if (digitalRead(BUTTON_2) == 1){  // Button 2 is pressed
     RefAmp = 1;
+    Serial.println(String("New Reference Value: ") + RefAmp); // Print RefAmp
   }
+
   if (RefAmp == 1.5 && AmpThrough < CURRENT_LIMIT)  // 
   {
-    if (AmpThrough > (RefAmp + 0.08)) {
-      b = curDuty;
-      b = b - 2;
-      b = constrain(b, 1, 191);  // Max %75 duty
-      curDuty = b;
-      Serial.println(b);
-      Serial.println(RefAmp);
-      Serial.println(AmpThrough);
-      analogWrite(PWM_PIN, b);
-      delay(400);
-    } else if (AmpThrough < (RefAmp - 0.08)) {
-      b = curDuty;
-      b = b + 2;
-      b = constrain(b, 1, 191);  // Max %75 duty
-      curDuty = b;
-      Serial.println(b);
-      Serial.println(RefAmp);
-      Serial.println(AmpThrough);
-      analogWrite(PWM_PIN, b);
-      delay(400);
+    if (AmpThrough > (RefAmp + 0.08)){
+      curDuty = constrain(curDuty - 2, 1, 191);  // Max %75 duty
+    } 
+    else if (AmpThrough < (RefAmp - 0.08)){
+      curDuty = constrain(curDuty + 2, 1, 191);  // Max %75 duty
     }
+    analogWrite(PWM_PIN, curDuty);
+    Serial.print(String("Reference Value: ") + RefAmp); // Print RefAmp
+    Serial.println(String("\nCurrent Value: ") + AmpThrough); // Print AmpThrough
+    Serial.println(String("\nDuty Cycle: ") + curDuty / 255); // Print curDuty (0-1)
+    delay(400);
   } 
   else if (RefAmp == 1 && AmpThrough < CURRENT_LIMIT)  // 1 A
   {
-    if (AmpThrough > (RefAmp + 0.05)) {
-      b = curDuty;
-      b = b - 2;
-      b = constrain(b, 1, 191);  // Max %75 duty
-      curDuty = b;
-      Serial.println(b);
-      Serial.println(RefAmp);
-      Serial.println(AmpThrough);
-      analogWrite(PWM_PIN, b);
-      delay(300);
-    } else if (AmpThrough < (RefAmp - 0.05)) {
-      b = curDuty;
-      b = b - 2;
-      b = constrain(b, 1, 191);  // Max %75 duty
-      curDuty = b;
-      Serial.println(b);
-      Serial.println(RefAmp);
-      Serial.println(AmpThrough);
-      analogWrite(PWM_PIN, b);
-      delay(300);
+    if (AmpThrough > (RefAmp + 0.05)){
+      curDuty = constrain(curDuty - 2, 1, 191);  // Max %75 duty
+    } 
+    else if (AmpThrough < (RefAmp - 0.05)){
+      curDuty = constrain(curDuty + 2, 1, 191);  // Max %75 duty
     }
+    analogWrite(PWM_PIN, curDuty);
+    Serial.print(String("Reference Value: ") + RefAmp); // Print RefAmp
+    Serial.println(String("\nCurrent Value: ") + AmpThrough); // Print AmpThrough
+    Serial.println(String("\nDuty Cycle: ") + curDuty / 255); // Print curDuty (0-1)
+    delay(300);
   } 
   else if (AmpThrough > CURRENT_LIMIT) {
     analogWrite(PWM_PIN, 0);  // Resets the duty cycle to zero
     delay(10000); // Wait for 10 seconds
     soft_start(); // Soft start again
+  }
 }
